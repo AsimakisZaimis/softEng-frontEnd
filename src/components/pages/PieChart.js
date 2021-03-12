2d6ee3e8c7e78c4b11a9a816427fdf4d37c6f9fb
AsimakisZaimis <AsimakisZaimis>
2021-03-12 18:49:27 +0200

    adding statistics

---
+++ src/components/pages/PieChart.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render } from "react-dom";
+import { Chart } from "react-google-charts";
+ 
+
+const pieOptions = {
+    title: "Συνολικές Φορτίσεις ανάλογα με την πόλη που βρίσκεται ο σταθμός",
+    pieHole: 0.5,
+    slices: [
+      {
+        color: "#2BB673"
+      },
+      {
+        color: "#d91e48"
+      },
+      {
+        color: "#007fad"
+      },
+      {
+        color: "#e9a227"
+      }
+    ],
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "233238",
+        fontSize: 14
+      }
+    },
+    tooltip: {
+      showColorCode: true
+    },
+    // chartArea: {
+    //   left: 0,
+    //   top: 0,
+    //   width: "100%",
+    //   height: "80%"
+    // },
+    fontName: "Roboto"
+  };
+export default  class PieChart extends React.Component {
+    state = {
+      chartImageURI: ""
+    };
+    render() {
+      return (
+        <div className="App">
+            <br></br>
+            <br></br>
+          <Chart
+            chartType="PieChart"
+            data={[["Age", "Weight"], ["Νομός Αττικής", 12], ["Νομός Θεσσαλονίκης", 5.5]]}
+            options={pieOptions}
+            graph_id="PieChart"
+            width={"100%"}
+            height={"400px"}
+            legend_toggle
+          />
+        </div>
+      );
+    }
+}
--- src/components/pages/Statistics.js
+++ src/components/pages/Statistics.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import '../../App.css';
+import '../pages/PieChart';
+import Chart from "react-google-charts";
+import ReactDOM from "react-dom";
+import PieChart from '../pages/PieChart';
+
 
 export default function Statistics() {
-  return <h1 className='statistics'>ΕΜΦΑΝΙΣΗ ΣΤΑΤΙΣΤΙΚΩΝ ΚΑΙ ΔΙΑΓΡΑΜΜΑΤΩΝ</h1>;
+
+
+  return (
+    <div className='stations'>
+        <h1 className='statistics'>ΕΜΦΑΝΙΣΗ ΣΤΑΤΙΣΤΙΚΩΝ ΚΑΙ ΔΙΑΓΡΑΜΜΑΤΩΝ</h1>
+        <PieChart></PieChart>
+    </div>
+  );
+
 }
+
+
+
